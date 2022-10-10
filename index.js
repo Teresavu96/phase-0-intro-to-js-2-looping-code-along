4e316f3b5f363e38205b5ee04f4bec3d94636c65
Anh Thu Vu <teresavu96@gmail.com>
2022-10-10 09:50:48 -0700

    commit tree aed1d0510564c3ec4030d6e0c081f1ca374375f8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+
+    let cart = [];
+    for (let i = 0; i < names.length; i++){
+        cart.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cart
+}
+function countDown(number){
+    let n = number;
+    while (n>=0){
+        console.log(n--);
+    }
+}
